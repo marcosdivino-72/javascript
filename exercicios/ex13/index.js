46e2346d164761a0a7a4fb534a89382e48838aaa
marcosdivino-72 <marcosdivino999@gmail.com>
2026-01-04 10:13:09 -0300

    lista

--- exercicios/ex13/index.js
+++ exercicios/ex13/index.js
@@ -1,9 +1,62 @@
 
 
+        
+        
+   const l = document.querySelector("#lista")
+   const b = document.querySelector("#button")
+   const text = document.querySelector("#textl")
+   const ul = document.querySelector("#lista")
+  
+   
+
+   b.addEventListener("click", add)
+ 
+
+  function add(){
+
+   let p = document.createElement('li')
+   p.addEventListener("click", marcado)
+   
+    function marcado(){
+
+         
+
+    
+   }
+   p.innerHTML="-"+text.value
+   text.value=""
+
+   ul.appendChild(p)
+          
+  p.addEventListener("dblclick", function remover(){
+
+    p.remove(p)
+   })
+   p.addEventListener("click", function marca(){
+
+    if(p.id=="marcado"){
+
+      p.style.textDecoration="none"
+      p.removeAttribute("id")
+    }
+    else{
+        p.setAttribute("id", "marcado")
+          p.style.textDecoration="line-through"
+    }
+
+    
+   })
 
 
- let f=n1=>{
+   }
+ 
+  
+   
+   
 
-    return n1
-}
-console.log(f(4 ))
+        
+        
+        
+        
+        
+        
