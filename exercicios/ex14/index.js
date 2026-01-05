8a97c221c5e5c9fa9b9a4f379096f3ef35eaa544
marcosdivino-72 <marcosdivino999@gmail.com>
2026-01-05 17:59:32 -0300

    manipulaçao de dados

--- exercicios/ex14/index.js
+++ exercicios/ex14/index.js
@@ -1,13 +1,29 @@
 
 
 
-addEventListener("keydown",(e)=>{
+addEventListener("keyup",(e)=>{
+
+let num=[0,1,2,3,4,5,6,7,8,9]
+numT=true 
+for( i of num){
+
+if(e.key==num[i]){
 
 
-if(e.key=="a"){
+    console.log("num")
+    document.querySelector("#num").value+=num[i]
+numT=false
 
-    console.log("espaço")
 }
 
 
-} )
+}if(numT==true){
+
+    document.querySelector("#text").value+=e.key
+}
+
+}
+
+
+
+)
