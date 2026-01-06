b44c57fa7a687d5ecf0f1f3ff5f9a95abfd1cfc1
marcosdivino-72 <marcosdivino999@gmail.com>
2026-01-06 16:30:04 -0300

    inicio do projeto calculadora

---
+++ D/calculadora/index.js
@@ -0,0 +1,71 @@
+
+let primeironum
+let tela=document.querySelector('#tela')
+
+addEventListener("keyup", (e)=>{
+
+
+
+let num =[Number(document.querySelector("#zero").innerHTML),Number(document.querySelector("#um").innerHTML),Number(document.querySelector("#dois").innerHTML),Number(document.querySelector("#tres").innerHTML),Number(document.querySelector("#quatro").innerHTML),Number(document.querySelector("#cinco").innerHTML),Number(document.querySelector("#seis").innerHTML),Number(document.querySelector("#sete").innerHTML),Number(document.querySelector("#oito").innerHTML),Number(document.querySelector("#nove").innerHTML)]
+
+
+
+for (i of num){
+
+    if(e.key==num[i]){
+
+
+     document.querySelector('#tela').value+=e.key
+     primeironum=true
+        break
+    }else{
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+let caractere =["+","-","*"]
+console.log(caractere[0])
+if(primeironum==true){
+for(i in caractere){
+ if(e.key==caractere[i]){
+ document.querySelector('#tela').value+=e.key
+primeironum=false
+break
+
+}
+
+}}
+
+
+console.log(Number(tela.value))
+
+//let tela = document.querySelector('#tela').value+=e.key
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+//Number(document.querySelector("#um")),Number(document.querySelector("#dois")),Number(document.querySelector("#tres")),Number(document.querySelector("quatro")),Number(document.querySelector("#cinco")),Number(document.querySelector("#seis")),Number(document.querySelector("#sete")),Number(document.querySelector("oito")),Number(document.querySelector("nove"))
