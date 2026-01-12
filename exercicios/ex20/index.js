6244b4a1e9a07eb8da5eba515949a1d68fa6501d
marcosdivino-72 <marcosdivino999@gmail.com>
2026-01-12 18:22:48 -0300

    snake

--- exercicios/ex20/index.js
+++ exercicios/ex20/index.js
@@ -1,18 +1,26 @@
 const canvas = document.querySelector("canvas")
+const score = document.querySelector("#score")
 const cxt=canvas.getContext("2d")
-cxt.fillStyle="red"
+
 const size =30
 let x =330
 let y =30
+let s=0
 
- let gerarComida=false
+ let gerarComida=true
 
 
-let = direction="direita"
- const snake=[{x:30,y:100},{x:60,y:120}]
+let  direction
+ const snake=[{x:30,y:120},{x:60,y:120}]
 
 const draw=()=>{
-    snake.forEach((snakePosition)=>{
+    
+    snake.forEach((snakePosition,i)=>{
+         cxt.fillStyle="#ffd"
+        if(snake[snake.length-1]==snake[i]){
+             cxt.fillStyle="rgba(255, 255, 255, 1)"
+        }
+       
         cxt.fillRect(snakePosition.x,snakePosition.y,size,size)
         
 
@@ -22,24 +30,12 @@ const draw=()=>{
 } 
 
 
-    document.body.addEventListener("keydown",(e)=>{
-        if(e.key=="w"&&direction!="baixo"){
-            
-            direction="cima"
-        }
-        if(e.key=="d"&&direction!="esquerda"){
-          direction="direita"
-        }
-        if(e.key=="s"&&direction!="cima"){
-          direction="baixo"
-        }if(e.key=="a"&&direction!="direita"){
-            direction="esquerda"
-        }
-    })
+   
 
  
 
 const moveSnake=()=>{
+    if(!direction)return
     const heand = snake[snake.length-1]
     if(heand.y==y && heand.x==x  ){
           gerarComida=true
@@ -66,38 +62,91 @@ snake.push({x:heand.x,y:heand.y+size})
 
 const comida=()=>{
 if(gerarComida==true){
-
+    
+    score.innerHTML=`SCORE:${s}`
+    s+=10
     x = Math.floor( Math.random()*20)*30
     y = Math.floor( Math.random()*20)*30
+    while(snake.find((position)=>position.x==x && position.y==y
+
+
+    )){
+         x = Math.floor( Math.random()*20)*30
+    y = Math.floor( Math.random()*20)*30
+    }
+  
 gerarComida=false
 }
-
-
+    cxt.shadowColor="blue"
+    cxt.fillStyle="blue"
+    cxt.shadowBlur=18
       cxt.fillRect(x,y,size,size)
+      cxt.shadowBlur=0
      
 }
 
-let intv= setInterval(()=>{
-let cab= snake[snake.length-1]
-    cxt.clearRect(0,0,600,600)
-   for(i=0;i<snake.length-1;i++){
+const gameover=()=>{
+    let cab= snake[snake.length-1]
+
+
+for(i=0;i<snake.length-1;i++){
      
-    if(cab.x==snake[i].x && cab.y==snake[i].y ){
-        
+    if(cab.x==snake[i].x && cab.y==snake[i].y || cab.x>570 ||cab.x<0||cab.y<0 ||cab.y>570){
+        const menu = document.querySelector("#menu")
+        menu.style.display="block"
+
         clearInterval(intv)
+        canvas.style.filter="blur(2px)"
        
 
        
     }
  
    }
+
+}
+
+let intv= setInterval(()=>{
+
+    cxt.clearRect(0,0,600,600)
    
-    
+   
+gameover()
 draw()
 moveSnake()
 comida()
 
-},300)  
+
+
+},300+s) 
+
+
+
+
+ document.body.addEventListener("keydown",(e)=>{
+      
+                
+              if(e.key=="w"&&direction!="baixo"){
+
+           
+            
+            direction="cima"
+        }
+        if(e.key=="d"&&direction!="esquerda"){
+          direction="direita"
+        }
+        if(e.key=="s"&&direction!="cima"){
+          direction="baixo"
+        }if(e.key=="a"&&direction!="direita"){
+            direction="esquerda"
+        }
+    }) 
+
+
+
+
+
+
 
 
 
