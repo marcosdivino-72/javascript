0c212b7cdbddcb37cb090e38ad5eb530f93c63b5
marcosdivino-72 <marcosdivino999@gmail.com>
2026-01-19 13:25:13 -0300

    treinamento

--- exercicios/ex220/index.js
+++ exercicios/ex220/index.js
@@ -1,9 +1,56 @@
 
-let frutas=["abacaxi", "uva","pera",'mamão']
+const itens = [...document.querySelectorAll(".item")]
+const btn = document.querySelector(".btn")
+let c=1
+  itens[0].style.transform= 'translateX(0)'
+ itens[1].style.transform= 'translateX(100%)'
+ itens[2].style.transform= 'translateX(200%)'
 
-let [,,x]=frutas
+btn.addEventListener("click",()=>{
 
-let coisas=[["abacaxi", "uva","pera",'mamão'],['maria', 'José']]
+  
+      if(c<=itens.length-1){
 
-let [,[,n2]]=coisas
-console.log(x)
+        switch(c){
+
+            case 0:{
+                 itens[0].style.transform= 'translateX(0)'
+ itens[1].style.transform= 'translateX(100%)'
+ itens[2].style.transform= 'translateX(200%)'
+                c++
+                break
+            }
+            case 1:{
+                 itens[0].style.transform= 'translateX(-100%)'
+ itens[1].style.transform= 'translateX(0%)'
+ itens[2].style.transform= 'translateX(100%)'
+   c++
+                break
+            }
+            case 2:{
+                 itens[0].style.transform= 'translateX(-200%)'
+ itens[1].style.transform= 'translateX(-100%)'
+ itens[2].style.transform= 'translateX(0%)'
+   c++
+                break
+            }
+        }
+
+
+      }   else{
+
+        itens.forEach((e)=>{
+            e.setAttribute("class","item t")
+
+        })
+        c=1
+         itens[0].style.transform= 'translateX(0)'
+ itens[1].style.transform= 'translateX(100%)'
+ itens[2].style.transform= 'translateX(200%)'
+    }
+ 
+
+
+
+    }
+)
