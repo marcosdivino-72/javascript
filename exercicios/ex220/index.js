a6dd61407a4eb83ca3b078445f6fa4deb38242fa
marcosdivino-72 <marcosdivino999@gmail.com>
2026-01-23 11:28:55 -0300

    treinamento

--- exercicios/ex220/index.js
+++ exercicios/ex220/index.js
@@ -1,56 +1,16 @@
 
-const itens = [...document.querySelectorAll(".item")]
-const btn = document.querySelector(".btn")
-let c=1
-  itens[0].style.transform= 'translateX(0)'
- itens[1].style.transform= 'translateX(100%)'
- itens[2].style.transform= 'translateX(200%)'
+const conteiner = document.querySelector("#conteiner")
+const itens =document.querySelectorAll(".item")
 
-btn.addEventListener("click",()=>{
-
-  
-      if(c<=itens.length-1){
-
-        switch(c){
-
-            case 0:{
-                 itens[0].style.transform= 'translateX(0)'
- itens[1].style.transform= 'translateX(100%)'
- itens[2].style.transform= 'translateX(200%)'
-                c++
-                break
-            }
-            case 1:{
-                 itens[0].style.transform= 'translateX(-100%)'
- itens[1].style.transform= 'translateX(0%)'
- itens[2].style.transform= 'translateX(100%)'
-   c++
-                break
-            }
-            case 2:{
-                 itens[0].style.transform= 'translateX(-200%)'
- itens[1].style.transform= 'translateX(-100%)'
- itens[2].style.transform= 'translateX(0%)'
-   c++
-                break
-            }
-        }
+let x=0
 
 
-      }   else{
+setInterval(()=>{
+x++;
+if(x>itens.length-1){
+  x=0
+}
+conteiner.style.transform=`translateX(${x*-500}px)`
+console.log(x)
 
-        itens.forEach((e)=>{
-            e.setAttribute("class","item t")
-
-        })
-        c=1
-         itens[0].style.transform= 'translateX(0)'
- itens[1].style.transform= 'translateX(100%)'
- itens[2].style.transform= 'translateX(200%)'
-    }
- 
-
-
-
-    }
-)
+},2000)
